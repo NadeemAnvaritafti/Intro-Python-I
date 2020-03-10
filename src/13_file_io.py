f87499de08d2f347b231fe7a57cf4a882b9c60f9
Nadeem <nanvari@live.com>
2020-03-09 23:13:08 -0500

    complete assignment for script 13

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -9,11 +9,21 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Print all the contents of the file, then close the file
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
-# YOUR CODE HERE
+f = open('src/foo.txt', 'r')
+print('--foo.txt file: ', f.read())
+f.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
-# YOUR CODE HERE
+f2 = open('src/bar.txt', 'w')
+f2.write('Hello there.\n')
+f2.write('General Kenobi.\n')
+f2.write('You are a bold one.\n')
+f2.close()
+
+f3 = open('src/bar.txt', 'r')
+print('--bar.txt: ', f3.read())
+f3.close()
